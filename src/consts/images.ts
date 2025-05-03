96c5dd52206b8e61adc13f2808735eeb69aa2627
Esteban Gonzalez Drummes12 <drummes.12@gmail.com>
2025-05-03 14:42:36 -0500

    Añade componente Hero; incluye imágenes para el carrusel

---
+++ src/consts/images.ts
@@ -0,0 +1,18 @@
+export const imagesHero = [
+  {
+    src: 'https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg',
+    alt: 'image-1'
+  },
+  {
+    src: 'https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg',
+    alt: 'image-2'
+  },
+  {
+    src: 'https://images.pexels.com/photos/1379636/pexels-photo-1379636.jpeg',
+    alt: 'image-3'
+  },
+  {
+    src: 'https://images.pexels.com/photos/3791466/pexels-photo-3791466.jpeg',
+    alt: 'image-4'
+  }
+]
