2a538eaec5e84c12082ce017436f53501974ebf7
Esteban Gonzalez Drummes12 <drummes.12@gmail.com>
2025-05-03 21:24:26 -0500

    Añade componente StyleLife y actualiza el índice para incluirlo; actualiza imágenes en el archivo de constantes

--- src/consts/images.ts
+++ src/consts/images.ts
@@ -19,19 +19,19 @@ export const imagesHero = [
 
 export const imagesWelcome = [
   {
-    src: 'https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg',
+    src: 'https://images.pexels.com/photos/1526713/pexels-photo-1526713.jpeg',
     alt: 'image-1'
   },
   {
-    src: 'https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg',
+    src: 'https://images.pexels.com/photos/2049422/pexels-photo-2049422.jpeg',
     alt: 'image-2'
   },
   {
-    src: 'https://images.pexels.com/photos/1379636/pexels-photo-1379636.jpeg',
+    src: 'https://images.pexels.com/photos/6658648/pexels-photo-6658648.png',
     alt: 'image-3'
   },
   {
-    src: 'https://images.pexels.com/photos/3791466/pexels-photo-3791466.jpeg',
+    src: 'https://images.pexels.com/photos/12683657/pexels-photo-12683657.jpeg',
     alt: 'image-4'
   }
 ]
