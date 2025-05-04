302fbd97c0ce87efbc7da378971446034a544e5a
Esteban Gonzalez Drummes12 <drummes.12@gmail.com>
2025-05-03 20:19:53 -0500

    Añade componentes Carousel y Section; implementa el componente Welcome y actualiza Hero para incluirlo; agrega imágenes para el carrusel y mejora el script de inicialización

--- src/consts/images.ts
+++ src/consts/images.ts
@@ -16,3 +16,22 @@ export const imagesHero = [
     alt: 'image-4'
   }
 ]
+
+export const imagesWelcome = [
+  {
+    src: 'https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg',
+    alt: 'image-1'
+  },
+  {
+    src: 'https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg',
+    alt: 'image-2'
+  },
+  {
+    src: 'https://images.pexels.com/photos/1379636/pexels-photo-1379636.jpeg',
+    alt: 'image-3'
+  },
+  {
+    src: 'https://images.pexels.com/photos/3791466/pexels-photo-3791466.jpeg',
+    alt: 'image-4'
+  }
+]
