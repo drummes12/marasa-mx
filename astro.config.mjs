cc3495f7352cd53e5666d26ec945e4ac8bae99c2
Esteban Gonzalez Drummes12 <drummes.12@gmail.com>
2025-05-24 14:12:24 -0500

    feat: actualiza configuración de salida en astro.config.mjs y añade adaptador de Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel'
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en']
   },
-  output: 'static'
-}) 
+  output: 'server',
+  adapter: vercel()
+})
