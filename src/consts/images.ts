ca7b40f4b96b93396f7714fd695e98ed79d4a64f
Esteban Gonzalez Drummes12 <drummes.12@gmail.com>
2025-05-27 12:20:19 -0500

    feat: añade nuevas imágenes y actualiza referencias en componentes

--- src/consts/images.ts
+++ src/consts/images.ts
@@ -1,80 +1,92 @@
 export const imagesHero = [
   {
-    src: 'https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg',
+    src: '/images/hero/hero-1.webp',
     alt: 'image-1'
   },
   {
-    src: 'https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg',
+    src: '/images/hero/hero-1.webp',
     alt: 'image-2'
   },
   {
-    src: 'https://images.pexels.com/photos/1379636/pexels-photo-1379636.jpeg',
+    src: '/images/hero/hero-1.webp',
     alt: 'image-3'
   },
   {
-    src: 'https://images.pexels.com/photos/3791466/pexels-photo-3791466.jpeg',
+    src: '/images/hero/hero-1.webp',
     alt: 'image-4'
   }
 ]
 
 export const imagesWelcome = [
   {
-    src: 'https://images.pexels.com/photos/1526713/pexels-photo-1526713.jpeg',
-    alt: 'image-1'
+    src: '/images/welcome/sala.webp',
+    alt: 'Sala de estar'
   },
   {
-    src: 'https://images.pexels.com/photos/2049422/pexels-photo-2049422.jpeg',
-    alt: 'image-2'
-  },
-  {
-    src: 'https://images.pexels.com/photos/6658648/pexels-photo-6658648.png',
-    alt: 'image-3'
-  },
-  {
-    src: 'https://images.pexels.com/photos/12683657/pexels-photo-12683657.jpeg',
-    alt: 'image-4'
+    src: '/images/welcome/puerto-vallarta-sunset-playa-los-arcos.webp',
+    alt: 'Atardecer en puerto vallarta'
   }
 ]
 
-export const imagesCommonsAreas = {
-  src: 'https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg',
-  alt: 'Imagen de área común'
+export const imagesLifeStyle = {
+  src: '/images/life-style.webp',
+  alt: 'Atardecer en puerto vallarta'
 }
 
-export const imagesLifeStyle = {
-  src: 'https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg',
-  alt: 'Imagen de estilo de vida'
+export const imagesUbication = {
+  src: '/images/mapa-marasa.webp',
+  alt: 'Mapa para encontrar Marasa'
+}
+
+export const imagesCommonsAreas = {
+  src: '/images/gallery/alberca.webp',
+  alt: 'Alberca de Marasa'
+}
+
+export const imagesInventory = {
+  src: '/images/conjunto.webp',
+  alt: 'Conjunto de Marasa'
 }
 
 export const imagesgallery = [
   {
-    src: 'https://images.pexels.com/photos/2049422/pexels-photo-2049422.jpeg',
-    alt: 'Imagen de galería 1',
-    tags: ['amenities']
-  },
-  {
-    src: 'https://images.pexels.com/photos/17832446/pexels-photo-17832446.jpeg',
-    alt: 'Imagen de galería 2',
-    tags: ['amenities']
-  },
-  {
-    src: 'https://images.pexels.com/photos/16105790/pexels-photo-16105790.jpeg',
-    alt: 'Imagen de galería 3',
-    tags: ['amenities']
-  },
-  {
-    src: 'https://images.pexels.com/photos/31837575/pexels-photo-31837575.jpeg',
-    alt: 'Imagen de galería 4',
+    src: '/images/gallery/coworking.webp',
+    alt: 'Coworking de Marasa',
     tags: ['commons-areas']
   },
   {
-    src: 'https://images.pexels.com/photos/32195239/pexels-photo-32195239.jpeg',
-    alt: 'Imagen de galería 5',
+    src: '/images/gallery/gym.webp',
+    alt: 'Gimnasio de Marasa',
     tags: ['commons-areas']
   },
   {
-    src: 'https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg',
-    alt: 'Imagen de galería 6',
+    src: '/images/gallery/fachada-frontal.webp',
+    alt: 'Fachada frontal de Marasa',
+    tags: ['amenities']
+  },
+  {
+    src: '/images/gallery/caseta.webp',
+    alt: 'Caseta de Marasa',
+    tags: ['amenities']
+  },
+  {
+    src: '/images/gallery/marasa.webp',
+    alt: 'Marasa',
+    tags: ['amenities']
+  },
+  {
+    src: '/images/gallery/marasa-vista.webp',
+    alt: 'Vista de Marasa',
+    tags: ['amenities']
+  },
+  {
+    src: '/images/gallery/salon-de-eventos.webp',
+    alt: 'Salón de eventos de Marasa',
+    tags: ['commons-areas']
+  },
+  {
+    src: '/images/gallery/alberca.webp',
+    alt: 'Alberca de Marasa',
     tags: ['commons-areas']
   }
 ]
