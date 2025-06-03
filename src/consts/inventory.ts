659b742504992327f3e371e31831769b62cfbd44
Esteban Gonzalez Drummes12 <drummes.12@gmail.com>
2025-06-03 12:47:01 -0500

    feat: actualiza modelos y traducciones en el componente de inventario, añade nueva imagen de fachada

--- src/consts/inventory.ts
+++ src/consts/inventory.ts
@@ -1,23 +1,26 @@
+import facadeCaracol from '@/assets/images/facades/fachada_frontal.jpg'
+import facadeCoral from '@/assets/images/facades/fachada_frontal.jpg'
+
 export const inventoryData = [
-  { id: 101, x: '25.5%', y: '34%', status: 'available', bedrooms: 4, size: '125.64 m2', price: '$7,737,000', image: 'CARACOL' },
-  { id: 102, x: '30%', y: '37.5%', status: 'reserved', bedrooms: 4, size: '137.13 m2', price: '$7,935,000', image: 'CORAL' },
-  { id: 103, x: '34.5%', y: '41.5%', status: 'sold', bedrooms: 4, size: '137.24 m2', price: '$7,938,000', image: 'CARACOL' },
-  { id: 104, x: '39.5%', y: '45.5%', status: 'sold', bedrooms: 4, size: '137.12 m2', price: '$7,938,000', image: 'CORAL' },
-  { id: 105, x: '44.5%', y: '49.5%', status: 'sold', bedrooms: 4, size: '136.8 m2', price: '$7,938,000', image: 'CARACOL' },
+  { id: 101, x: '25.5%', y: '34%', status: 'available', bedrooms: 4, size: '125.64 m2', price: '$7,737,000', image: facadeCaracol, model: 'caracol' },
+  { id: 102, x: '30%', y: '37.5%', status: 'reserved', bedrooms: 4, size: '137.13 m2', price: '$7,935,000', image: facadeCoral, model: 'coral' },
+  { id: 103, x: '34.5%', y: '41.5%', status: 'sold', bedrooms: 4, size: '137.24 m2', price: '$7,938,000', image: facadeCaracol, model: 'caracol' },
+  { id: 104, x: '39.5%', y: '45.5%', status: 'sold', bedrooms: 4, size: '137.12 m2', price: '$7,938,000', image: facadeCoral, model: 'coral' },
+  { id: 105, x: '44.5%', y: '49.5%', status: 'sold', bedrooms: 4, size: '136.8 m2', price: '$7,938,000', image: facadeCaracol, model: 'caracol' },
 
-  { id: 106, x: '61.5%', y: '68%', status: 'reserved', bedrooms: 4, size: '200.23 m2', price: '$9,048,000', image: 'CARACOL' },
-  { id: 107, x: '64%', y: '62%', status: 'sold', bedrooms: 4, size: '139.94 m2', price: '$7,980,000', image: 'CORAL' },
-  { id: 108, x: '67%', y: '55%', status: 'available', bedrooms: 4, size: '139.91 m2', price: '$7,980,000', image: 'CARACOL' },
-  { id: 109, x: '69.5%', y: '49%', status: 'reserved', bedrooms: 4, size: '139.88 m2', price: '$7,980,000', image: 'CORAL' },
-  { id: 110, x: '72%', y: '43.4%', status: 'available', bedrooms: 4, size: '139.85 m2', price: '$7,980,000', image: 'CARACOL' },
-  { id: 111, x: '74%', y: '38%', status: 'available', bedrooms: 4, size: '139.84 m2', price: '$7,980,000', image: 'CORAL' },
-  { id: 112, x: '76.5%', y: '32.5%', status: 'available', bedrooms: 4, size: '139.88 m2', price: '$7,980,000', image: 'CARACOL' },
-  { id: 113, x: '78.5%', y: '28%', status: 'reserved', bedrooms: 4, size: '193.98 m2', price: '$8,650,000', image: 'CORAL' },
+  { id: 106, x: '61.5%', y: '68%', status: 'reserved', bedrooms: 4, size: '200.23 m2', price: '$9,048,000', image: facadeCaracol, model: 'caracol' },
+  { id: 107, x: '64%', y: '62%', status: 'sold', bedrooms: 4, size: '139.94 m2', price: '$7,980,000', image: facadeCoral, model: 'coral' },
+  { id: 108, x: '67%', y: '55%', status: 'available', bedrooms: 4, size: '139.91 m2', price: '$7,980,000', image: facadeCaracol, model: 'caracol' },
+  { id: 109, x: '69.5%', y: '49%', status: 'reserved', bedrooms: 4, size: '139.88 m2', price: '$7,980,000', image: facadeCoral, model: 'coral' },
+  { id: 110, x: '72%', y: '43.4%', status: 'available', bedrooms: 4, size: '139.85 m2', price: '$7,980,000', image: facadeCaracol, model: 'caracol' },
+  { id: 111, x: '74%', y: '38%', status: 'available', bedrooms: 4, size: '139.84 m2', price: '$7,980,000', image: facadeCoral, model: 'coral' },
+  { id: 112, x: '76.5%', y: '32.5%', status: 'available', bedrooms: 4, size: '139.88 m2', price: '$7,980,000', image: facadeCaracol, model: 'caracol' },
+  { id: 113, x: '78.5%', y: '28%', status: 'reserved', bedrooms: 4, size: '193.98 m2', price: '$8,650,000', image: facadeCoral, model: 'coral' },
 
-  { id: 114, x: '46%', y: '26.5%', status: 'available', bedrooms: 4, size: '168.08 m2', price: '$8,397,000', image: 'CORAL' },
-  { id: 115, x: '40.3%', y: '24.5%', status: 'available', bedrooms: 4, size: '165.06 m2', price: '$8,378,000', image: 'CARACOL' },
-  { id: 116, x: '34.5%', y: '22.5%', status: 'available', bedrooms: 4, size: '164.46 m2', price: '$8,376,000', image: 'CORAL' },
-  { id: 117, x: '29.2%', y: '21%', status: 'available', bedrooms: 4, size: '163.92 m2', price: '$8,373,000', image: 'CARACOL' },
-  { id: 118, x: '23.7%', y: '19%', status: 'available', bedrooms: 4, size: '163.49 m2', price: '$8,371,000', image: 'CORAL' },
-  { id: 119, x: '18.5%', y: '18%', status: 'available', bedrooms: 4, size: '161.48 m2', price: '$8,342,000', image: 'CARACOL' }
+  { id: 114, x: '46%', y: '26.5%', status: 'available', bedrooms: 4, size: '168.08 m2', price: '$8,397,000', image: facadeCoral, model: 'coral' },
+  { id: 115, x: '40.3%', y: '24.5%', status: 'available', bedrooms: 4, size: '165.06 m2', price: '$8,378,000', image: facadeCaracol, model: 'caracol' },
+  { id: 116, x: '34.5%', y: '22.5%', status: 'available', bedrooms: 4, size: '164.46 m2', price: '$8,376,000', image: facadeCoral, model: 'coral' },
+  { id: 117, x: '29.2%', y: '21%', status: 'available', bedrooms: 4, size: '163.92 m2', price: '$8,373,000', image: facadeCaracol, model: 'caracol' },
+  { id: 118, x: '23.7%', y: '19%', status: 'available', bedrooms: 4, size: '163.49 m2', price: '$8,371,000', image: facadeCoral, model: 'coral' },
+  { id: 119, x: '18.5%', y: '18%', status: 'available', bedrooms: 4, size: '161.48 m2', price: '$8,342,000', image: facadeCaracol, model: 'caracol' }
 ]
